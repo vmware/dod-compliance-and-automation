3ea32580f239b7b814f9757a6b3510bdcd01d30d
Nitin Ravindran <nravindran@pivotal.io>
2021-07-03 23:54:36 -0400

    add V-237769.rb

---
+++ ubuntu/18.04/inspec/canonical-ubuntu-18.04-lts-stig-baseline/controls/V-237769.rb
@@ -0,0 +1,54 @@
+# encoding: UTF-8
+
+control 'V-237769' do
+  title "All local interactive user home directories must have mode 0750 or
+less permissive."
+  desc  "Excessive permissions on local interactive user home directories may
+allow unauthorized access to user files by other users."
+  desc  'rationale', ''
+  desc  'check', "
+    Verify the assigned home directory of all local interactive users has a
+mode of \"0750\" or less permissive with the following command:
+
+    Note: This may miss interactive users that have been assigned a privileged
+User Identifier (UID). Evidence of interactive use may be obtained from a
+number of log files containing system logon information.
+
+    $ sudo ls -ld $(awk -F: '($3>=1000)&&($7 !~ /nologin/){print $6}'
+/etc/passwd)
+
+    drwxr-x--- 2 smithj admin 4096 Jun 5 12:41 smithj
+
+    If home directories referenced in \"/etc/passwd\" do not have a mode of
+\"0750\" or less permissive, this is a finding.
+  "
+  desc  'fix', "
+    Change the mode of interactive user’s home directories to \"0750\". To
+change the mode of a local interactive user’s home directory, use the following
+command:
+
+    Note: The example will be for the user \"smithj\".
+
+    $ sudo chmod 0750 /home/smithj
+  "
+  impact 0.5
+  tag severity: 'medium'
+  tag gtitle: 'SRG-OS-000480-GPOS-00227'
+  tag gid: 'V-237769'
+  tag rid: 'SV-237769r648743_rule'
+  tag stig_id: 'UBTU-18-010451'
+  tag fix_id: 'F-40942r648738_fix'
+  tag cci: ['CCI-000366']
+  tag legacy: []
+  tag nist: ['CM-6 b']
+
+  local_user_home_dirs=command('awk -F: \'($3>=1000)&&($7 !~ /nologin/){print $6}\' /etc/passwd').stdout.strip.split("\n").entries
+  pwck_output=command('pwck -r').stdout
+
+  local_user_home_dirs.each do |dir|
+    describe directory(dir) do
+      it { should_not be_more_permissive_than("0750") }
+    end
+  end
+end
+
