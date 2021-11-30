471aa44c359fb8d276eafc8c0e7d944c60da7be7
rlakey <rlakey81@outlook.com>
2021-11-29 18:48:15 -0700

    updated logic in firewall service name exceptions

--- vsphere/7.0/vsphere/powercli/VMware_vSphere_7.0_STIG_ESXi_Remediation.ps1
+++ vsphere/7.0/vsphere/powercli/VMware_vSphere_7.0_STIG_ESXi_Remediation.ps1
@@ -35,9 +35,9 @@ param (
     [string]$vcenter,
     [Parameter(Mandatory=$true)]
     [pscredential]$vccred,
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory=$true,ParameterSetName="hostname")]
     [string]$hostname,
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory=$true,ParameterSetName="cluster")]
     [string]$cluster,
     [Parameter(Mandatory=$true,
     HelpMessage="Enter the Active Directory Admins group to use for administrative access to ESXi")]
@@ -1691,8 +1691,8 @@ Try{
         $Title = "The ESXi host must configure the firewall to restrict access to services running on the host."
         Write-ToConsole "...Remediating Vulnerability ID:$VULID STIG ID:$STIGID with Title: $Title"
         ForEach($vmhost in $vmhosts){
-            #vSphere Web Client and the Dell VxRail services are excluded from the script due to the order PowerCLI does firewall rules which removes all allowed IPs briefly before setting new allowed ranges which breaks connectivity from vCenter to ESXi so these must be manually done.
-            $fwservices = $vmhost | Get-VMHostFirewallException | Where-Object {($_.Enabled -eq $True -and $_.extensiondata.allowedhosts.allip -eq "enabled") -and ($_.Name -ne "vSphere Web Client" -xor $_.Name -ne "dellptagenttcp" -xor $_.Name -ne "dellsshServer")}
+            #vSphere Web Client, VMware vCenter Agent, and the Dell VxRail services are excluded from the script due to the order PowerCLI does firewall rules which removes all allowed IPs briefly before setting new allowed ranges which breaks connectivity from vCenter to ESXi so these must be manually done.
+            $fwservices = $vmhost | Get-VMHostFirewallException | Where-Object {($_.Enabled -eq $True) -and ($_.extensiondata.allowedhosts.allip -eq "enabled") -and ($_.Name -ne "vSphere Web Client") -and ($_.Name -ne "dellptagenttcp") -and ($_.Name -ne "dellsshServer") -and ($_.Name -ne "VMware vCenter Agent")}
             $esxcli = Get-EsxCli -VMHost $vmhost -V2
             ForEach($fwservice in $fwservices){
                 $fwsvcname = $fwservice.extensiondata.key
