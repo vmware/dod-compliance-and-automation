f1fc3ff4195258c4ae70649f57e3bb1da76744f0
Ryan <rlakey81@outlook.com>
2023-06-01 21:02:16 -0600

    fixing folders and adding ci

--- vcf/5.x/ansible/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Change Log
-
-## [5.0 Version 1 Release 1] (2023-06-01)
-
-#### Release Notes
-- Initial release
--- vcf/5.x/inspec/controls/photon.rb
+++
@@ -1,4 +0,0 @@
-include_controls 'photon' do
-  # NTP handled by SDDC Manager UI
-  skip_control 'PHTN-30-000058'
-end
