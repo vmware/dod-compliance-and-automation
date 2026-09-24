f9f337b096af8f1b38f677069c9c9a911a0ed2ff
darrickw <darrick.williams@broadcom.com>
2026-09-24 14:32:54 -0500

    backport std switch fix to other versions

--- vsphere/8.0/v1r1-srg/vsphere/inspec/vmware-vsphere-8.0-stig-baseline/esxi/controls/ESXI-80-000216.rb
+++ vsphere/8.0/v1r1-srg/vsphere/inspec/vmware-vsphere-8.0-stig-baseline/esxi/controls/ESXI-80-000216.rb
@@ -74,11 +74,11 @@ control 'ESXI-80-000216' do
 
   if !vmhosts.empty?
     vmhosts.each do |vmhost|
-      command = "Get-VMHost -Name #{vmhost} | Get-VirtualSwitch | Get-SecurityPolicy | Select-Object -ExpandProperty ForgedTransmits"
+      command = "Get-VMHost -Name #{vmhost} | Get-VirtualSwitch -Standard | Get-SecurityPolicy | Select-Object -ExpandProperty ForgedTransmits"
       describe powercli_command(command) do
         its('stdout.strip') { should_not match 'True' }
       end
-      command = "Get-VMHost -Name #{vmhost} | Get-VirtualPortGroup | Get-SecurityPolicy | Select-Object -ExpandProperty ForgedTransmits"
+      command = "Get-VMHost -Name #{vmhost} | Get-VirtualPortGroup -Standard | Get-SecurityPolicy | Select-Object -ExpandProperty ForgedTransmits"
       describe powercli_command(command) do
         its('stdout.strip') { should_not match 'True' }
       end
